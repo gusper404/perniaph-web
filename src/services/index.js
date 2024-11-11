a68e16c846aca5427e0ef749001f0e6fc3d0f9e1
Marcel Pernia <marcelpm1987@gmail.com>
2024-11-11 18:52:06 +0100

    se refactoriza para centralizar queries en unico archivo y se instala dependencia para colorear sintaxis groq

---
+++ src/services/index.js
@@ -0,0 +1,32 @@
+import { sanityClient } from "sanity:client";
+import groq from "groq"
+
+export const getPosts = async () => {
+  return await sanityClient.fetch(groq`*[_type == 'photography'] {
+    _id,
+    _createdAt,
+    image {
+      asset->{
+        url
+      }
+    },
+    category->{
+      _id,
+      title
+    },
+    location->{
+      _id,
+      title
+    },
+    model->{
+      fullname,
+      rrss
+    }
+  } | order(publishedAt desc)`);
+}
+
+export const getSettings = async () => {
+  return await sanityClient.fetch(groq`*[_id == 'settings'][0] {
+    title
+  }`)
+}
